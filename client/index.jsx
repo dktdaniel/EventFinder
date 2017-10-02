4a0c268c4a451bcd12db4e17ee334127ea8402a8
Hack Reactor Student <student@hackreactor.com>
2017-10-02 14:01:19 -0700

    Attempt to get first google maps wrapper to work

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const Search = require('./components/Search.jsx');
+const Container = require('./components/Container.jsx');
 
 class App extends React.Component {
   constructor(props) {
@@ -12,6 +13,7 @@ class App extends React.Component {
       <div>
         <h1>Occa</h1>
         <Search />
+        <Container />
       </div>
     )
   }

d2156d0367a8b96fa3309ac96f457a018e853e25
Casey Hebebrand <caseyhebebrand@gmail.com>
2017-10-03 13:29:46 -0700

    use Promise chain to search DB or API

--- server/index.js
+++ server/index.js
@@ -4,30 +4,63 @@ const bodyParser = require('body-parser');
 const ticketmaster = require('./ticketmaster.js');
 const db = require('../db/index.js');
 
-const app = Express(); 
+const app = Express();
 
 app.use(bodyParser.json());
 
 //Serve up static html
+app.use(express.static(__dirname + '/../client'));
+
 
 
 app.get('/', (req, res) => {
-  
+
   var requestBody = {
     lat: 37.788799,
     lng: -122.394798,
     rad: '5'
   };
+   var range = 0.0145 * requestBody.rad;
+   var options = {
+     lat: requestBody.lat,
+     lng: requestBody.lng,
+     range: range
+   };
 
-  ticketmaster.getEvents(requestBody)
-  .then((processedData) => {
-    console.log(JSON.stringify(processedData));
-    res.status(200).send(processedData);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-  
+  db.searchEvents(options)
+    .then(events => {
+      if (events.length !== 0) {
+        throw events;
+      }
+      return ticketmaster.getEvents(requestBody)
+    })
+    .then(apiEvents => {
+      return Promise.all(apiEvents.map( event => {
+        return db.searchOrCreateVenue(event.venue)
+              .then( id => {
+                event.event.venueId = id
+              })
+              .then( () => {
+                db.addNewEvents(event)
+              })
+      }))
+
+    })
+    .then( events => {
+      res.send(events)
+    })
+    .catch( events => {
+      res.send(events)
+    })
+  // ticketmaster.getEvents(requestBody)
+  // .then((processedData) => {
+  //   console.log(JSON.stringify(processedData));
+  //   res.status(200).send(processedData);
+  // })
+  // .catch((err) => {
+  //   console.error(err);
+  // });
+
 })
 
 app.listen(3000, () => {

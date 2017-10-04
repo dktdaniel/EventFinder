194432b6349fbde76a81c97168a94cd44fbfc195
Joe Liotta <joe.liotta00@gmail.com>
2017-10-03 19:13:24 -0700

    merge completed, adding index.js back in

---
+++ server/index.js
@@ -0,0 +1,73 @@
+const Express = require('express');
+const Promise = require('bluebird');
+const bodyParser = require('body-parser');
+const ticketmaster = require('./ticketmaster.js');
+const db = require('../db/index.js');
+const cors = require('cors');
+
+const app = Express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+//Serve up static html
+app.use(express.static(__dirname + '/../client'));
+
+
+
+app.get('/events', (req, res) => {
+
+  var requestBody = req.body || {
+    lat: 37.788799,
+    lng: -122.394798,
+    rad: 5
+  };
+
+   var range = 0.0145 * requestBody.rad;
+   var options = {
+     center_lat: requestBody.lat,
+     center_lng: requestBody.lng,
+     range: range
+   };
+
+  db.searchEvents(options)
+    .then(events => {
+      if (events.length !== 0) {
+        throw events;
+      }
+      return ticketmaster.getEvents(requestBody)
+    })
+    .then(apiEvents => {
+      return Promise.all(apiEvents.map( event => {
+        return db.searchOrCreateVenue(event.venue)
+        .then( id => {
+          console.log('IS THE ID COMING BACK?', id);
+          console.log('After ID, we add ID to event:', event);
+          event.event.venueId = id;
+          return db.addNewEvents(event.event);
+        });
+      }))
+
+    })
+    .then( events => {
+      console.log('Returning this to client:', events);
+      res.send(events)
+    })
+    .catch( events => {
+      console.log('What am I catching?', events);
+      res.send(events)
+    })
+  // ticketmaster.getEvents(requestBody)
+  // .then((processedData) => {
+  //   console.log(JSON.stringify(processedData));
+  //   res.status(200).send(processedData);
+  // })
+  // .catch((err) => {
+  //   console.error(err);
+  // });
+
+})
+
+app.listen(3000, () => {
+  console.log('Listening on Port 3000!');
+});

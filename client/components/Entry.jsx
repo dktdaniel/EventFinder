7febfd422cf7eeee4ef19e5d276c45e7096bc0e2
Casey Hebebrand <caseyhebebrand@gmail.com>
2017-10-05 14:27:27 -0700

    format event date & time

--- client/components/Entry.jsx
+++ client/components/Entry.jsx
@@ -5,6 +5,52 @@ class Entry extends React.Component {
 	constructor(props) {
 		super(props);
 		console.log('PROPS:', this);
+		this.state = {
+			formattedTime: '',
+			formattedDate: ''
+		}
+		this.formatTime = this.formatTime.bind(this);
+	}
+
+	componentDidMount() {
+		this.formatTime();
+		this.formatDate();
+	}
+
+	formatTime() {
+		var time = this.props.event.event.startTime;
+		var hours = Number(time.slice(0, 2));
+		var mins = Number(time.slice(3,5));
+		var ampm;
+
+		if(hours > 12) {
+			hours = (hours - 12).toString();
+			ampm = 'pm';
+		} else if (hours < 0) {
+			hours = (12).toString();
+			ampm = 'am';
+		} else if (hours < 12){
+			ampm = 'am';
+		}
+
+		if (mins < 10) {
+			mins = "0" + mins;
+		}
+
+		this.setState({
+			formattedTime: hours + ":" + mins + ampm
+		})
+	};
+
+	formatDate() {
+		var date = this.props.event.event.startDate;
+		var month = date.slice(5, 7);
+		var day = date.slice(8, 10);
+		var year = date.slice(0, 4);
+
+		this.setState({
+			formattedDate: month + "/" + day + "/" + year
+		})
 	}
 
 	render() {
@@ -12,8 +58,8 @@ class Entry extends React.Component {
 			<Item>
 	      <Item.Content>
 	        <Item.Header>{this.props.event.event.name}</Item.Header>
-	        <Item.Meta>{this.props.event.event.startDate}</Item.Meta>
-	        <Item.Meta>{this.props.event.event.startTime}</Item.Meta>
+	        <Item.Meta>{this.state.formattedDate}</Item.Meta>
+	        <Item.Meta>{this.state.formattedTime}</Item.Meta>
 	        <Item.Description>
 	          <Image src={this.props.event.event.image} />
 	        </Item.Description>

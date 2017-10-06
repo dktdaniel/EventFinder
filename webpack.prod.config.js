c3cdf1cd0a397e9c5bca59768f59fccd1067ca73
Ja-on John Kim <jaonjohnkim@github.com>
2017-10-06 15:22:53 -0700

    Prep for Heroku deployment

---
+++ webpack.prod.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client/dist')
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};

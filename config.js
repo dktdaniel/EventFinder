28025cfaa4f229fe81b81cfb5e6910886363734d
Ja-on John Kim <jaonjohnkim@github.com>
2017-10-06 16:57:43 -0700

    JawsDB config url update

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
   API_KEY: 'AtOwigrmCrAspfzDj7jShDwdTfFsfjRf',
   KEY: 'AIzaSyC491LEkdjt5HkmZp4lTOzyd2nUj6jIKCg'
+  JAWSDB_PUCE_URL: 'mysql://hefxi9cqilt3skii:tu0j6m7znbzy7a6u@lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/yo88eizmx9t13cyi'
 }

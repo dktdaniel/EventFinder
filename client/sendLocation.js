8c5871fd14757e81e4df758c99f47e2c14c7aa68
Ja-on John Kim <jaonjohnkim@github.com>
2017-10-06 17:43:23 -0700

    Changed reference to heroku link and not localhost

--- client/sendLocation.js
+++ client/sendLocation.js
@@ -12,7 +12,7 @@ const eventTypes = {
 
 var actions = {
   get: function(google, map, cb) {
-    return $.ajax('http://localhost:3000/events')
+    return $.ajax('https://occa.heroku.com/' + process.env.PORT + '/events')
     .then(data => {
       return this._prepMarkers(data, cb, google, map)
       .then(markers => {
@@ -24,7 +24,7 @@ var actions = {
   post: (lat, lng, google, map, cb) => {
     return $.ajax({
       method: 'POST',
-      url: 'http://localhost:3000/events',
+      url: 'https://occa.heroku.com/' + process.env.PORT + '/events',
       data: {
         data: JSON.stringify({
           lat: lat,

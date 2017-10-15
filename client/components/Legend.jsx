0e0312b91084db782f59cdb282bea828df66b321
Daniel <dktdaniel@gmail.com>
2017-10-15 13:42:59 -0700

    Restyling sidebar display

--- client/components/Legend.jsx
+++ client/components/Legend.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
+import { Container, Image, Header, Grid, Icon, Dimmer, Segment } from 'semantic-ui-react';
 
 class Legend extends React.Component {
-	constructor(props) {
-		super(props);
-		this.markers = Object.keys(this.props.markers);
-	}
+    constructor(props) {
+        super(props);
+        this.markers = Object.keys(this.props.markers);
+    }
 
-	render() {
-		return (
-			<div id="legend"> Sort By These:
-				<ul id="legendList">
-					{this.markers.map((type, key) =>
-						<li onClick={() => this.props.sort(type)} key={key}><img src={this.props.markers[type]} /> {type} </li>
-					)}
-				</ul>
-			</div>
-		)
-	}
+    render() {
+        return (<Container fluid>
+            <div id="legend"> <Header as='h4' id='sort'>Sort by:</Header>
+                <ul id="legendList">
+                    {this.markers.map((type, key) =>
+                        <li onClick={() => this.props.sort(type)} key={key}><img src={this.props.markers[type]} /> {type} </li>
+                    )}
+                </ul>
+            </div>
+    </Container>)
+    }
 }
 
 export default Legend;
